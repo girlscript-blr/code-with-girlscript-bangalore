876036408bf20dcc594164b779ebda6c7caf52f0
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2020-07-09 19:27:20 +0530

    feat: add delivery_charges

--- Medium/1. Shopping Cart Problem/solutions/chttrjeankr_Ankur_Chattopadhyay/utilities.py
+++ Medium/1. Shopping Cart Problem/solutions/chttrjeankr_Ankur_Chattopadhyay/utilities.py
@@ -26,3 +26,8 @@ shop_details = {
     "shopAddress": "311/5 Akshay nagar, Bangalore, Karnataka, India",
     "shopContactNumber": "+91 9988776655",
 }
+delivery_cost = {
+    range(0, 6): 0,
+    range(6, 21): 30,
+    range(21, 51): 60,
+}
