b0b641f059c5cc66fc1067aaed01ab68967d1129
damini31 <63578387+damini31@users.noreply.github.com>
2020-07-17 13:52:45 +0530

    Update ShoppingCart_Easy.cpp

--- Easy/1. Shopping Cart Problem/solutions/damini31_DaminiChauhan/ShoppingCart_Easy.cpp
+++ Easy/1. Shopping Cart Problem/solutions/damini31_DaminiChauhan/ShoppingCart_Easy.cpp
@@ -1,74 +1,67 @@
 #include <iostream>
 #include<string.h>
 using namespace std;
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 
-void calculate(string a,int f)
+void calculate(int a,int f)
 {
-    map<string,int> cost{{"Samsung wireless headset", 2000}, 
-            {"MI wireless headset", 1000},{"Apple Airpods", 12000},
-            {"Boat headset", 2000},{ "Noise headset", 1500}}; 
-   
+    map<int,int> Product{{101, 2000},
+            {102, 1000},{108, 12000},
+            {109, 2000},{110, 1500}};
+
+    map<int,string> name{{101,"Samsung wireless headset"},
+            {102,"MI wireless headset"},{108,"Apple Airpods"},
+            {109,"Boat headset"},{110,"Noise headset"}};
+
    string b[]={"Samsung wireless headset","MI wireless headset","Apple Airpods","Boat headset","Noise headset"};
-  
-   
+
+
    int x,n_bill,t_bill;
-   for(int i=0;i<5;i++)
-   {
-       int x = a.compare(b[i]);
-       if(x==0)
-       {
-           n_bill = f * cost[a];
+
+
+           n_bill = f * Product[a];
            /*6% on the total bill */
            t_bill= n_bill + n_bill*0.06;
            cout<<"\n-------------------Amount to be paid---------------------";
            cout<<"\n"<<"Product"<<"                      "<<"Quantity"<<"    "<<"Amount per piece";
            cout<<"\n----------------------------------------------------------";
-           cout<<"\n"<<a<<"            "<<f<<"          "<<cost[a];
+           cout<<"\n"<<name[a]<<"            "<<f<<"          "<<Product[a];
            cout<<"\n";
            cout<<"\nTotal amount(before tax):"<<"         "<<n_bill;
            cout<<"\n6% tax on the above amount:"<<"       "<<n_bill*0.06;
            cout<<"\nTotal amount to be paid:"<<"          "<<t_bill;
-           break;
-          
-       }
-       
-   } 
 
-}
 
-void calculate(string a,int b);
+
+   }
+
+void calculate(int a,int b);
 int main()
 {
-  string name,phno,payment,x;
-  int y;
-  
+  string name,phno,payment;
+  int x,y;
+
   cout<<"\n---------------Welcome   to   GadgetifyWithGSBlr-------------------";
-  
+
   cout<<"\nEnter the customer's name ";
   getline(cin,name);
   cout<<"\nEnter the customer's Phone number";
   getline(cin,phno);
   cout<<"\nChoose the Payment method - (cash/card/online)";
   getline(cin,payment);
-  cout<<"\nSelect a single shopping item and it’s quantity\n";
-  getline(cin,x);
-  cin>>y;
+
   cout<<"\n------------LIST OF ITEMS-----------------\n";
-  cout<<"1. Samsung wireless headset: Rs.2000\n";
-  cout<<"2. MI wireless headset: Rs.1000\n";
-  cout<<"3. Apple Airpods: Rs.12000\n";
-  cout<<"4. Boat headset: Rs.2000\n";
-  cout<<"5. Noise headset: Rs.1500\n";
-  
-  
-  
-  /*removing space from the last character*/
-  if(!x.empty())
-  {
-      x.resize(x.size()-1);
-  }
- 
+  cout<<"\nProduct ID         Product Name               Price";
+  cout<<"101                Samsung wireless headset:    Rs.2000\n";
+  cout<<"102                MI wireless headset:         Rs.1000\n";
+  cout<<"108                Apple Airpods:               Rs.12000\n";
+  cout<<"109                Boat headset:                Rs.2000\n";
+  cout<<"110                Noise headset:               Rs.1500\n";
+
+  cout<<"\nSelect a single shopping item by the product ID and it’s quantity\n";
+  cin>>x;
+  cin>>y;
+
   cout<<"\n---------------------Total Bill----------------------";
   cout<<"\n**************GagdetifyWithGSBLr********************";
   cout<<"\n        Shop name: GadgetifyWithGSBlr       ";
@@ -81,10 +74,10 @@ int main()
   cout<<"\n               Product Quantity: "<<y;
   calculate(x,y);
 
- time_t my_time = time(NULL); 
+ time_t my_time = time(NULL);
  cout<<"\n\nBilling Date & Time:"<<ctime(&my_time);
  cout<<"----------------Have A Great Day------------------------";
-   
+
 
  return 0;
 }
