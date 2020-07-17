1d246b5fca1a2188023a6e8553d72ab07794c406
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2020-07-17 11:18:44 +0530

    feat: register sites to admin

--- Hard/1. Shopping Cart Problem/solutions/chttrjeankr_Ankur_Chattopadhyay/shoppingcart/admin.py
+++ Hard/1. Shopping Cart Problem/solutions/chttrjeankr_Ankur_Chattopadhyay/shoppingcart/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from shoppingcart.models import Item, Category, ItemInOrder, Order
 
 # Register your models here.
+
+admin.site.register(Item)
+admin.site.register(ItemInOrder)
+admin.site.register(Category)
+admin.site.register(Order)
