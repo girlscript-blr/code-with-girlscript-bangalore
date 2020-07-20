434fbeb00da8d0daf73868f926a9c9ef24309e25
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2020-07-21 03:13:38 +0530

    feat: add admin site customization
    * Item and Category can be added, viewed,
    modified by Vendor
    * Order and IteminOrder can be just 
    viewed by Vendor

--- Hard/1. Shopping Cart Problem/solutions/chttrjeankr_Ankur_Chattopadhyay/shoppingcart/admin.py
+++ Hard/1. Shopping Cart Problem/solutions/chttrjeankr_Ankur_Chattopadhyay/shoppingcart/admin.py
@@ -1,10 +1,85 @@
+from datetime import datetime
+
 from django.contrib import admin
 
 from shoppingcart.models import Category, Item, ItemInOrder, Order
 
-# Register your models here.
 
-admin.site.register(Item)
-admin.site.register(ItemInOrder)
-admin.site.register(Category)
-admin.site.register(Order)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    actions = ["mark_as_complete", "mark_as_cancelled", "mark_as_in_transit"]
+    list_display = (
+        "pk",
+        "order_status",
+        "billing_date_time",
+        "order_modified",
+        "amount_payable",
+        "delivery_option",
+        "payment_method",
+    )
+    list_filter = ("order_status", "delivery_option")
+
+    def mark_as_complete(self, request, queryset):
+        queryset.update(order_status="COMP", order_modified=datetime.now())
+
+    mark_as_complete.short_description = "Mark selected orders as completed"
+
+    def mark_as_cancelled(self, request, queryset):
+        queryset.update(order_status="CANC", order_modified=datetime.now())
+
+    mark_as_cancelled.short_description = "Mark selected orders as cancelled"
+
+    def mark_as_in_transit(self, request, queryset):
+        queryset.update(order_status="TRAN", order_modified=datetime.now())
+
+    mark_as_in_transit.short_description = "Mark selected orders as in transit"
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "name",
+        "category",
+        "original_price",
+        "discount_price",
+        "weight_in_gms",
+        "available",
+    )
+    list_filter = ("category", "available")
+
+
+@admin.register(ItemInOrder)
+class ItemInOrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "item",
+        "order",
+        "quantity",
+    )
+    list_filter = ("order",)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "name",
+    )
