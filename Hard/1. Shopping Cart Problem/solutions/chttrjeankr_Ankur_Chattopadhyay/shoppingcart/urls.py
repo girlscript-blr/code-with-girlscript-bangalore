ad160dc76ffca44ec08520750985a09c1a73780e
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2020-07-21 03:09:02 +0530

    feat: add new urls
    * new for categories and order status
    * add an int field for shopping list

--- Hard/1. Shopping Cart Problem/solutions/chttrjeankr_Ankur_Chattopadhyay/shoppingcart/urls.py
+++ Hard/1. Shopping Cart Problem/solutions/chttrjeankr_Ankur_Chattopadhyay/shoppingcart/urls.py
@@ -6,6 +6,12 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("add_to_cart/", views.add_to_cart, name="add_to_cart"),
     path("clear_cart/", views.clear_cart, name="clear_cart"),
-    path("shopping_list/", views.display_shopping_list, name="display_shopping_list"),
+    path("categories/", views.display_categories, name="categories"),
+    path(
+        "shopping_list/<int:category>/",
+        views.display_shopping_list,
+        name="display_shopping_list",
+    ),
     path("order/", views.create_order, name="create_order"),
+    path("status/", views.get_order_status, name="order_status"),
 ]
