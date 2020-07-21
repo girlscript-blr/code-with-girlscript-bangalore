e19a787d791763198fc4683fe2b4dbb4dc09509a
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2020-07-21 14:49:26 +0530

    chore: remove static shopping_list

--- Hard/1. Shopping Cart Problem/solutions/chttrjeankr_Ankur_Chattopadhyay/shoppingcart/utilities.py
+++ Hard/1. Shopping Cart Problem/solutions/chttrjeankr_Ankur_Chattopadhyay/shoppingcart/utilities.py
@@ -1,26 +1,3 @@
-shopping_list = {
-    "boAt BassHeads 100 in-Ear Wired Earphones": {
-        "original_price": 600,
-        "discount_price": 399,
-        "weight_in_gms": 67,
-    },
-    "Bose SoundSport Wireless Earbuds": {
-        "original_price": 14999,
-        "discount_price": 13275,
-        "weight_in_gms": 100,
-    },
-    "Kindle (10th Gen; Black; 6-Inch)": {"original_price": 7999, "weight_in_gms": 174,},
-    "MI 10000mAh Power Bank": {
-        "original_price": 1199,
-        "discount_price": 899,
-        "weight_in_gms": 350,
-    },
-    "OnePlus 7 Pro": {
-        "original_price": 52999,
-        "discount_price": 42999,
-        "weight_in_gms": 206,
-    },
-}
 shop_details = {
     "shopName": "GadgetifyWithGSBlr",
     "shopAddress": "311/5 Akshay nagar, Bangalore, Karnataka, India",
