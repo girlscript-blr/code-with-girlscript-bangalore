3e5cac6d4f96750cbfd1df33415904712c2131fb
Vanshikaa <vanshika31032000@gmail.com>
2020-11-03 13:01:11 +0530

    update1

--- Easy/5. IPL Management App/solutions/Vanshikaa00_Vanshika/AppEasy.java
+++ Easy/5. IPL Management App/solutions/Vanshikaa00_Vanshika/AppEasy.java
@@ -3,27 +3,34 @@ public class AppEasy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter the number of teams playing the tournament: ");
-int n=sc.nextInt();
-String name,captain,franchise,hg;
-Map<Integer,Details> list=new LinkedHashMap<Integer,Details>();
-for(int i=0;i<n;i++) {
-	System.out.println("Fill team "+(i+1)+" details: ");
-	System.out.println("Enter team name: ");
-	name=sc.next();
-	System.out.println("Enter captain name: ");
-	captain=sc.next();
-	System.out.println("Enter franchise: ");
-	franchise=sc.next();
-	System.out.println("Enter home ground: ");
-	hg=sc.next();
-	list.put(i+1,new Details(name,captain,franchise,hg));
-}
+		Scanner sc=new Scanner(System.in);
+		int n;
+		String name,captain,franchise,hg;
+		Map<Integer,Details> list=new LinkedHashMap<Integer,Details>();
+		System.out.println("Enter the number of teams playing the tournament: ");
+		try {
+			n=sc.nextInt();
+			for(int i=0;i<n;i++) {
+				System.out.println("Fill team "+(i+1)+" details: ");
+				System.out.println("Enter team name: ");
+				name=sc.next();
+				System.out.println("Enter captain name: ");
+				captain=sc.next();
+				System.out.println("Enter franchise: ");
+				franchise=sc.next();
+				System.out.println("Enter home ground: ");
+				hg=sc.next();
+				list.put(i+1,new Details(name,captain,franchise,hg));
+			}
+		}
+		catch(Exception e) {
+			System.err.println("Please provide correct value!");
+			main(new String[0]);
+		}
 
-for(Map.Entry map:list.entrySet()) {
-	System.out.println(map.getKey()+" "+map.getValue());
-}
+		for(Map.Entry map:list.entrySet()) {
+			System.out.println(map.getKey()+" "+map.getValue());
+		}
 
 	}
 
--- Easy/5. IPL Management App/solutions/Vanshikaa00_Vanshika/Details.java
+++ Easy/5. IPL Management App/solutions/Vanshikaa00_Vanshika/Details.java
@@ -16,6 +16,6 @@ public class Details {
 	}
 
 
-	
+
 
 }
