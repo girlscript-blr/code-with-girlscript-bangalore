d4e471f030d26b82cd673bfa918ca3f315ddeb5b
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2020-11-30 23:46:28 +0530

    solution modified as specified

--- Easy/5. IPL Management App/solutions/sakshi300699_Sakshi_Bhandarkar/IPL_Easy_Sol.cpp
+++ Easy/5. IPL Management App/solutions/sakshi300699_Sakshi_Bhandarkar/IPL_Easy_Sol.cpp
@@ -1,30 +1,48 @@
+#include <bits/stdc++.h>
 #include <iostream>
 
 using namespace std;
-
+bool isNumber(string s)
+{
+    for (int i = 0; i < s.length(); i++)
+        if (isdigit(s[i]) == false && s[i]!='.')
+            return false;
+    return true;
+}
 int main()
 {
 
-   int num;
+   string n;
 
    cout<<"Number of Teams playing the tournament:";
-   cin>>num;
-   string name[num+1], captain[num+1], franchise[num+1], home_ground[num+1];
+   cin>>n;
+
+    while(!isNumber(n)){
+        cout<<"Retry!";
+        cin>>n;
+    }
+    int num=stoi(n);
+    cin.ignore();
+
+    string name[num+1], captain[num+1], franchise[num+1], home_ground[num+1];
    for(int i=1;i<=num;i++){
     cout<<"\nFill Team"<<i<<" Details:";
     cout<<"\nName:";
-    cin>>name[i];
+    getline(cin,name[i]);
     cout<<"\nCaptain:";
-    cin>>captain[i];
+    getline(cin,captain[i]);
     cout<<"\nFranchise:";
-    cin>>franchise[i];
+    getline(cin,franchise[i]);
     cout<<"\nHome Ground:";
-    cin>>home_ground[i];
+    getline(cin,home_ground[i]);
 
    }
    cout<<"\nSI No.\t\tTeam Name\tCaptain\t\tFranchise\tHome Ground\n";
    for(int i=1;i<=num;i++){
     cout<<i<<"\t\t"<<name[i]<<"\t\t"<<captain[i]<<"\t\t"<<franchise[i]<<"\t\t"<<home_ground[i]<<"\n";
    }
+
+
     return 0;
 }
+
